0ff4c02c3100f427cda06625d4a6f4bd6c5ef363
gounaya <o.gounaya@gmail.com>
2020-06-30 18:14:38 +0200

    add avatar

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js');  // Call discord app 
 let bot = new Discord.Client();
 
-bot.login("NzI3NDc0ODgyMTM2NTA2NDE5.XvsaXw.RLnRNMfd8lpSffr2m4v6RyN7w-s");
+bot.login("NzI3NDc0ODgyMTM2NTA2NDE5.XvtHUw.nxmmkiRPFDQl0rsLRYtEwQuQHF0");
 
 
 bot.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setAvatar('./public/pictures/aio-bot-avis.jpg')
+        .then(() => console.log('Avatar mis en place avec succès'))
+        .catch(console.error)
+    bot.user.setGame('detecting ...')
+        .then(() => console.log('Avatar mis en place avec succès'))
+        .catch(console.error)
 });
 
 
