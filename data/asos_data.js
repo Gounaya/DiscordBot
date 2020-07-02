212e611ee9247d9c8972c2199f93d59cc8fd95eb
gounaya <o.gounaya@gmail.com>
2020-07-02 12:19:52 +0200

    get all jordan items from ASOS, well done

--- data/asos_data.js
+++ data/asos_data.js
@@ -1,5 +1,74 @@
+const Discord = require('discord.js');  // Call discord app 
+const fetch = require('node-fetch');
+const snekfetch = require('snekfetch');
+const aj_api = 'https://www.asos.com/api/product/search/v2/categories/5775?brand=14269&channel=desktop-web&country=FR&currency=EUR&keyStoreDataversion=j42uv2x-26&lang=fr-FR&limit=72&offset=0&rowlength=4&store=FR';
+const api = 'https://jsonplaceholder.typicode.com/posts';
+var fs = require('fs');
 
 
-const url = 'https://www.nike.com/us/launch/t/kobe-4-protro-wizenard/';
-const brand = '';
-const methode = '';
+module.exports.run = async (bot, message, args) => {
+    var list_data = [];
+    var list_data_history_data = [];
+    var new_in = [];
+
+    snekfetch.get(aj_api)
+        .then(
+            r => {
+
+                let data_size = r.body.products.length;
+
+                fs.writeFile('history.json', JSON.stringify(r.body.products, null, 2), (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    };
+                    console.log("File has been created");
+                });
+
+                //switch data from json to a table
+                for (let i = 0; i < data_size; i++) {
+
+                    let item_data = {
+                        id: r.body.products[i].id,
+                        name: r.body.products[i].name,
+                        url: 'https://www.asos.com/fr/' + r.body.products[i].url,
+                        price: r.body.products[i].price.current.text,
+                        imageUrl: r.body.products[i].imageUrl,
+                        productCode: r.body.products[i].productCode,
+                    }
+                    list_data_history_data.push(r.body.products[i].productCode);
+                    list_data.push(item_data);
+
+                    console.log(list_data[i].price);
+                    console.log("-----------------------");
+                    console.log(r.body.products[i]);
+                    console.log("-----------------------");
+
+                    const exampleEmbed = new Discord.MessageEmbed()
+                        .setColor('#0xF1C40F')  //0099ff
+                        .setTitle(list_data[i].name)
+                        .setURL(list_data[i].url)
+                        .setThumbnail('https://i.imgur.com/5wXoCeQ.png')
+                        .addField("Price: ", list_data[i].price)
+                        //.setThumbnail(list_data[i].imageUrl)
+                        .setAuthor('ASOS Tracker', 'https://static-pepper.dealabs.com/merchants/avatar_web_square_94/avatar/64_3.jpg', 'https://www.asos.com/fr/homme/')
+                        .setTimestamp()
+                        .setFooter('created by @Osmedge', 'https://pbs.twimg.com/media/D0HLKHKXcAE6Uqe?format=jpg&name=large');
+                    message.channel.send(exampleEmbed);
+                }
+
+               
+
+                //return list_data;
+
+            }
+        ).catch((error) => {
+            console.log(error);
+        });
+
+};
+
+module.exports.help = {
+    name: 'json'
+};
+
--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const Discord = require('discord.js');  // Call discord app
 let bot = new Discord.Client();
 const AjCommand = require('./commands/aj_command');
 const YzyCommand = require('./commands/yzy_command');
+const asosdata = require('./data/asos_data');
+const fetch = require('node-fetch');
+
+const url = 'https://www.asos.com/api/product/search/v2/categories/17184?brand=14269&channel=desktop-web&country=FR&currency=EUR&keyStoreDataversion=j42uv2x-26&lang=fr-FR&limit=72&offset=0&rowlength=4&store=FR';
 
 
-bot.login("NzI3NDc0ODgyMTM2NTA2NDE5.Xvtldw.ByuWZ11muLPgYmsCE_-keEBBuyY");
+//bot.login("NzI3NDc0ODgyMTM2NTA2NDE5.XvxALQ.ql8EqIZ0_coITEc5bxZ8edquD7g");
 
 
 bot.on('ready', () => {
@@ -33,11 +37,14 @@ bot.on('guildMemberAdd', function(member){
 
 bot.on('message', function (message) {
 
+
     if(String(message) === '!ajgetall'){
         AjCommand.parse(message);
     }else if (String(message) === '!yzygetall'){
         YzyCommand.parse(message);
+    }else if (String(message) === 'asos'){
+        console.log("asos ==> ");
+        asosdata.run(bot, message);
     }
-
 });
 
